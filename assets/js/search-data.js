02f6a796f653c4fc444e745e22d0b7cacac5b9f5
carolissima <156902923+carolissima@users.noreply.github.com>
2026-02-12 14:30:21 +0000

    Deploying to gh-pages from @ cogsci-mannheim/csi@83d1c55504dda0ad9026a24ccb2eff9b9c9b272e 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -138,17 +138,6 @@ ninja.data = [{
             window.location.href = "/csi/blog/2024/post-citation/";
           
         },
-      },{id: "post-a-post-with-pseudo-code",
-        
-          title: "a post with pseudo code",
-        
-        description: "this is what included pseudo code could look like",
-        section: "Posts",
-        handler: () => {
-          
-            window.location.href = "/csi/blog/2024/pseudocode/";
-          
-        },
       },{id: "post-a-post-with-jupyter-notebook",
         
           title: "a post with jupyter notebook",
